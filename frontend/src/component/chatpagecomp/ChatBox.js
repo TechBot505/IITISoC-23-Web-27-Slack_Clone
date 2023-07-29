0a578c14d813302c1c07d0d924f420ffb22df870
PranjalJaiswal-IITI <mems210005033@iiti.ac.in>
2023-07-29 22:02:04 +0530

    ok

--- frontend/src/component/chatpagecomp/ChatBox.js
+++ frontend/src/component/chatpagecomp/ChatBox.js
@@ -15,7 +15,7 @@ const ChatBox = ({fetchAgain, setFetchAgain}) => {
   bg="white"  
   w=  { {base: "100%" , md:"68% "}}
   borderRadius="lg" 
-  borderWidth="1px"
+  borderWidth="2px"
   >
     
     <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
